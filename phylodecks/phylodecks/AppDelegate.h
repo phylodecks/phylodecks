118045883fe0fde7e95bcbaa7444e20ea3237d98
phylodecks <phylodecks@gmail.com>
2013-06-01 22:26:04 -0700

    cocos2d framework chosen

---
+++ phylodecks/phylodecks/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  phylodecks
+//
+//  Created by Sun, You Fei on 13-06-01.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ phylodecks/phylodecks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  phylodecks
+//
+//  Created by Sun, You Fei on 13-06-01.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
